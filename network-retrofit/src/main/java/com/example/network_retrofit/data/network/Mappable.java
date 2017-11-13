74d2aaef462bc84296df4700768d0040104a752d
Maxim Stepanov <ms@m4bank.com>
2017-11-13 10:43:57 +0300

    123

--- network-retrofit/src/main/java/com/example/network_retrofit/data/network/Mappable.java
+++
@@ -1,8 +0,0 @@
-package com.example.network_retrofit.data.network;
-
-/**
- * Created by mamba on 02.10.2017.
- */
-
-public interface Mappable {
-}
--- network-retrofit/src/main/java/com/example/network_retrofit/data/network/miscellaneous/enums/TransactionMoneyInteractionType.java
+++
@@ -1,9 +0,0 @@
-package com.example.network_retrofit.data.network.miscellaneous.enums;
-
-/**
- * Created by mamba on 02.10.2017.
- */
-
-public enum TransactionMoneyInteractionType {
-    UNKNOWN, CARD, CASH, ALIPAY, ECOM
-}
